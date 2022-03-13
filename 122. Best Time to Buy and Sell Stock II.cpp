c458bd9a387910673a55866729bfcfd704dfc837
Disha167 <khattridisha14@gmail.com>
2022-03-13 03:32:36 -0700

    11th Jan 2022 Ques 2

--- 122. Best Time to Buy and Sell Stock II.cpp
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -4,6 +4,21 @@ everytime the adjacent next price is greater than the present.
 
 If you will make a graph for the prices like 1 7 8 10, then the differences between the consecutive numbers 7-1=6, 8-7=1, 10-8=2 all will sum up to 6+2+1=9=10-1
 always so you can add the differences everytime.
+
+
+The peak valley concept goes like this:
+1
+\       3   /5   
+ \     /\  /
+  \   /  \/
+   \ /   4
+    2
+  HEIGHT (2,3)+HEIGHT(4,5)>=HEIGHT(2,5)
+  
+  There cant be a possible value of stocks where this is not true because how much the stocks will go down from 3 to 4, this much it will go up and extra will be 
+  height(4,5)-height(3,4) so we have this diff extra in L.H.S , it can't be less .
+  
+  
 */
 
 
