03e29e295af209c3e56b7199fa5582ba545e05a9
Disha167 <khattridisha14@gmail.com>
2022-06-19 09:26:44 -0700

    18th June 2022 Ques 1

--- 138. Copy List with Random Pointer.cpp
+++ 138. Copy List with Random Pointer.cpp
@@ -1,7 +1,12 @@
 //🎯DAY 85 PROBLEM 1
 /*Here we have to create a hard copy of brand new n nodes that has same numerical values as that of the previous list, and the new nodes should have same
 same connection as that of previous list. 
-THE APPROACH TO THIS PROBLEM IS
+THE APPROACH TO THIS PROBLEM IS to use a hashmap to map all the nodes with the newly created hard copies & then while traversing the linked list, make
+mp[curr_node]->next=mp[curr_node->next] i.e. hardcopy of curr_node can be retreived by map & it's next will point to hardcopy of (curr_node's->next).
+Similarly for random connection
+& If we want to reduce the space complexity, then instead of using map to keep the reference of original node's hardcopy for all the nodes, we can use
+the next pointer of original linked list and populate it with all hard copies & then use it for making the random & next connections similarly.
+After making the connections, the links of original nodes to hard copy can be deleted to preserve the original list.
 */
 // Definition for a Node.
 class Node {
