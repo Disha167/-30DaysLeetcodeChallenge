2937f1b2186bccd930e15a52f5355b86437e59a3
Disha167 <khattridisha14@gmail.com>
2024-04-14 22:50:22 +0530

    binary search concept

--- 274. H-Index.cpp
+++ 274. H-Index.cpp
@@ -1,23 +1,21 @@
-/*The goal of this question is to find the largest m such that we have atleast m numbers greater than m. So, for easily getting the count of numbers greater than a a particular
-number for calculating the answer, we will sort the array. 
-Note: It can also be solved linearly, not a very intuitive binary search question but can think of it to optimize
-Then, we can apply binary search to find the value of m optimally, as we can always neglect one side of the array left or right and decide to move in one direction based on the
-current number in an array
-Eg:   0 1 2 5 6
-Index 0 1 2 3 4
-So start=0 end=4, mid = 0+4/2=2 we will check whether at 2nd index, we have an eligible h-index?, i.e. 2 so n-mid=5-2=3 so 2<3 it cannot be an h-index as we do not have 3 numbers that are
-greater than 3, so we need to increase the citation so we will move towards right i.e. start++. 
-Now, start=1 end=4, mid=1+4/2=3 at 3rd index we have 5, 5>=(5-3) or 5>=2 so there are two numbers i.e. greater than 2, so ans=2 for now. For optimising the answer, we will do end-- as we can 
-find a more optimised answer at left i.e. comparatively greater vaue of n-mid as mid decreases.
-Now start=1 end=3 mid=1+3/2=2, again start++
-Now start=2 end=3 mid=3+2/2=2 again start++
-Now start=3 end=3 mid=3 then end--
-start>end and it comes out of loop*/
-
-
-
 class Solution {
 public:
+/*
+
+quality|  /
+       | /
+       |/_________
+       quantity
+
+      1) humko ye 45 degree ki line ke paas laana hai, agar quantity kam hai mtlb lowerside me hai toh quanity badhaenge mtlb left me move kro
+       taaki 45 degree tangent line ke paas jaye.. 
+       citations[i] quality hai n-mid quantity
+       citations[i]>=n-mid hai toh hum end=mid-1 left me jaenge
+      2) agar quality kam hai toh mtlb upperside me hai toh hum right me move krenge array me taaki wo 45 degree line ke paas aaye mtlb ratio
+       quality/quantity badh jaye kyunki array increasing order me hai toh
+       citations[i] quality hai, agar ye kam hai toh start=mid+1 right me jane keliye
+*/
+
     int hIndex(vector<int>& citations) {
         int n=citations.size()-1;
         sort(citations.begin(),citations.end());
@@ -28,14 +26,18 @@ public:
        while(start<=end)
        {
         int mid=start + (end-start)/2;
-        if(citations[mid]>=((end-mid)+1))
+        if(citations[mid]>=((n-mid)+1))
         {
-            ans=max(ans,min(citations[mid],((n-mid)+1)));// imp to use n here as end value changes everytime and to get all numbers greater than current you will need fixed size
+            ans=max(ans,min(citations[mid],((n-mid)+1)));
+            cout<<mid<<" "<<ans<<endl;
+            end=mid-1; /*citations[mid] mtlb quality hai toh agar bada hai quality toh hum quantity badhaenge mtlb left me search krenge
+            */
+
            
-            end--;
         }
-        else
-        start++;
+        else/*idhar humari quantity zyada hai toh hum right me jaenge kyunki
+        hume quality badhana hai aur array increasing order me order me sorted hai*/
+         start=mid+1;
        }
        return ans;
 
